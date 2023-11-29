8f500cdc98ca68e0066cddc8d801eeac992f850e
Michel Metran <michelmetran@gmail.com>
2023-11-29 13:35:07 -0300

    Ajustes

--- my_script.py
+++ my_script.py
@@ -1,1 +1,20 @@
+"""
+Summary
+"""
+
+import os
+from pathlib import Path
+
+project_path = Path(__file__).parent
+post_path = project_path / 'collections' / '_posts'
+
+if not post_path.is_dir():
+    raise Exception(f'{post_path} is not a directory')
+
+list_ipynb = list(post_path.rglob('*/*.ipynb'))
+
+for ipynb in list_ipynb:
+    print(ipynb)
+
+
 print('Helloooooo')
